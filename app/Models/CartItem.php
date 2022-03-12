05c1a13b7f1c5008f99f3970453426da547ee566
tatun55 <tatun55@gmail.com>
2022-03-13 08:25:16 +0900

    some fix

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class CartItem extends Model
 {
-    use HasFactory;
-
     protected $primaryKey = 'id';
     protected $keyType = 'string';
     public $incrementing = false;
