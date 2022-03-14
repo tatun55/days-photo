3c0ac6f32e8983fabd918d1b1a411f7815d5a88c
tatun55 <tatun55@gmail.com>
2022-03-14 15:04:31 +0900

    some fix

--- resources/views/pages/user/home.blade.php
+++ resources/views/pages/user/home.blade.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="row pt-4 pt-md-0">
                 <div class="mx-2"><small><span class="text-danger">*</span> 現在のバージョンは、α版です ( <a class="text-gray-600 text-decoration-underline" href="https://days-photo.s3.ap-northeast-1.amazonaws.com/days.+%E3%80%9C%E3%81%8B%E3%82%93%E3%81%9F%E3%82%93%E3%83%95%E3%82%A9%E3%83%88%E7%AE%A1%E7%90%86%E3%80%9C+%CE%B1%E7%89%88%E4%BD%BF%E3%81%84%E6%96%B9.pdf" target="_blank" rel="noopener noreferrer">詳しい使い方をPDFでみる</a>)</small></div>
-                <div class="mx-2"><small><span class="text-danger">*</span> ぜひアンケートへの回答をお願いします ( <a class="text-gray-600 text-decoration-underline" href="https://forms.gle/AeQLv4qGNQkbNfDy9" target="_blank" rel="noopener noreferrer">アンケートに回答</a>)</small></div>
+                <div class="mx-2"><small><span class="text-danger">*</span> ぜひご利用後にアンケート回答をお願いします ( <a class="text-gray-600 text-decoration-underline" href="https://forms.gle/AeQLv4qGNQkbNfDy9" target="_blank" rel="noopener noreferrer">アンケートに回答</a>)</small></div>
 
                 @include('sections.profile-card')
 
